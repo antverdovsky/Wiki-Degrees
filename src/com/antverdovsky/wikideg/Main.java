a21e20a79e98ccb2023b34af4a684112499f3f91
Anatoly Tverdovsky <antverdovsky@gmail.com>
2017-05-01 15:29:37 -0500

    Implemented Method for Fetching All Backlinks
    Implemented a method for fetching all backlinks of a Wikipedia article,
    given its title.

--- src/com/antverdovsky/wikideg/Main.java
+++ src/com/antverdovsky/wikideg/Main.java
@@ -9,21 +9,45 @@ public class Main {
 	public static void main(String[] args) {
 		ArrayList<ArrayList<String>> boLinks = 
 				new ArrayList<ArrayList<String>>();
+		ArrayList<ArrayList<String>> boBacklinks = 
+				new ArrayList<ArrayList<String>>();
 		
+		double a = System.currentTimeMillis();
 		try {
 			 boLinks = WikiAPI.getAllLinks("Barack Obama");
+			 boBacklinks = WikiAPI.getAllBacklinks("Barack Obama");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		double b = System.currentTimeMillis();
+		
+		int totalBoLinks = 0;
+		int totalBoBacklinks = 0;
 		
 		for (ArrayList<String> listN : boLinks) {
 			System.out.println("List Size: " + listN.size());
 			
 			for (String s : listN) {
+				totalBoLinks++;
 				System.out.println("\t" + s);
 			}
 			
 			System.out.println("\n");
 		}
+		
+		for (ArrayList<String> listN : boBacklinks) {
+			System.out.println("List Size: " + listN.size());
+			
+			for (String s : listN) {
+				totalBoBacklinks++;
+				System.out.println("\t" + s);
+			}
+			
+			System.out.println("\n");
+		}
+		
+		System.out.println("Total Links: " + totalBoLinks);
+		System.out.println("Total Backlinks: " + totalBoBacklinks);
+		System.out.println("Time taken: " + (b - a));
 	}
 }
--- src/com/antverdovsky/wikideg/WikiAPI.java
+++ src/com/antverdovsky/wikideg/WikiAPI.java
@@ -7,6 +7,44 @@ import com.antverdovsky.wikideg.WikiParse.LinksResult;
 
 public class WikiAPI {
 	/**
+	 * Gets all of the backlinks on a Wikipedia article of the specified name.
+	 * @param article The name of the article.
+	 * @return A 2D List of all of the backlinks. Each row in the list 
+	 *         contains a list of article titles, with at most 500 articles
+	 *         per list.
+	 * @throws IOException If the article data could not be fetched.
+	 */
+	public static ArrayList<ArrayList<String>> getAllBacklinks(String article)
+			throws IOException {
+		// Declare the 2D List of all of the links
+		ArrayList<ArrayList<String>> allLinks = 
+				new ArrayList<ArrayList<String>>();
+
+		// Get the URL of the Wiki Request Page and fetch the first JSON file
+		String url = WikiFetch.getBacklinksURL(article);
+		String json = WikiFetch.getData(url);
+
+		// Set the result to a null list and a continue token of zero (first
+		// page).
+		LinksResult result = new LinksResult(null, "0|0");
+		
+		// Continue fetching JSON files until we reach a file which contains
+		// no continue token. For each JSON file parsed, add its links to the
+		// all links 2D list.
+		while (!result.getLContinue().isEmpty()) {
+			// Get the JSON data of the new URL (with the continue token)
+			url = WikiFetch.getBacklinksURL(article, result.getLContinue());
+			json = WikiFetch.getData(url);
+
+			// Parse the JSON data from the URL and add it to the list
+			result = WikiParse.parseBacklinks(json);
+			allLinks.add(result.getLinks());
+		}
+
+		return allLinks; // Return all of the links when done
+	}
+	
+	/**
 	 * Gets all of the forward links on a Wikipedia article of the specified
 	 * name.
 	 * @param article The name of the article.
