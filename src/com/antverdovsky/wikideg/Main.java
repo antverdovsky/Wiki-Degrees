ae89fc8483a81ff082ad822cb629ce4da98e9a84
Anatoly Tverdovsky <antverdovsky@gmail.com>
2017-05-02 21:09:13 -0500

    Started Writing Main Algorithm
    Started writing the main algorithm for determining the degrees of
    separation and path between two Wikipedia articles. For now, only the
    special cases (start == end, start -> end, start -> middle -> end) have
    been implemented.

--- src/com/antverdovsky/wikideg/Main.java
+++ src/com/antverdovsky/wikideg/Main.java
@@ -2,52 +2,110 @@ package com.antverdovsky.wikideg;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Stack;
 
 import com.antverdovsky.wikideg.WikiParse.LinksResult;
 
 public class Main {
-	public static void main(String[] args) {
-		ArrayList<ArrayList<String>> boLinks = 
-				new ArrayList<ArrayList<String>>();
-		ArrayList<ArrayList<String>> boBacklinks = 
-				new ArrayList<ArrayList<String>>();
+	private static class Separation {
+		int numDegrees;          // Degrees of Separation
+		Stack<String> path;      // The path from start to end
 		
-		double a = System.currentTimeMillis();
+		/**
+		 * Creates a new instance of the Separation class with the specified
+		 * number of degrees of separation and the specified path.
+		 * @param numD The number of degrees of separation.
+		 * @param path The path from the starting link to the ending link.
+		 */
+		public Separation(int numD, Stack<String> path) {
+			this.numDegrees = numD;
+			this.path = path;
+		}
+		
+		/**
+		 * Returns the number of degrees of separation between two articles.
+		 * @return The degrees of separation.
+		 */
+		public int getNumDegrees() {
+			return this.numDegrees;
+		}
+		
+		/**
+		 * Returns the path between two articles.
+		 * @return The path.
+		 */
+		public Stack<String> getPath() {
+			return this.path;
+		}
+	}
+	
+	/**
+	 * Finds the degrees of separation and path between the starting and 
+	 * ending articles.
+	 * @param start The starting article title.
+	 * @param end The ending article title.
+	 * @throws IOException If there occurs an error fetching the articles of
+	 *                     either the starting or ending article. 
+	 */
+	private static Separation findConnection(String start, String end) 
+			throws IOException {
+		// Empty Stack to store the path between start and end
+		Stack<String> path = new Stack<String>();
+		path.push(start);
+		
+		// Special case if start == end -> Return 0 degrees of separation
+		// and a path containing the starting article.
+		if (start.equals(end)) return new Separation(0, path);
+		
+		// Fetch the starting and ending links
+		ArrayList<ArrayList<String>> startLinks2D = WikiAPI.getAllLinks(start);
+		ArrayList<ArrayList<String>> endLinks2D = WikiAPI.getAllBacklinks(end);
+		
+		// TODO: pass the reference to the arraylist to the WikiAPI so that
+		// we do not have to deal with a 2D list and so that we don't need to
+		// waste CPU power joining the links together.
+		ArrayList<String> startLinks = new ArrayList<String>();
+		ArrayList<String> endLinks = new ArrayList<String>();
+		for (ArrayList<String> aS : startLinks2D) startLinks.addAll(aS);
+		for (ArrayList<String> aS : endLinks2D) endLinks.addAll(aS);
+		
+		// Special case if the end article is in the links of the starting
+		// article, or the start article is in the backlinks of the end 
+		// article (start -> end).
+		if (startLinks.contains(end)) {
+			path.push(end);
+			return new Separation(1, path);
+		}
+		
+		// Special case if there is some article that is in both the starting
+		// links and the ending backlinks (start -> middle -> end).
+		ArrayList<String> common = new ArrayList<String>(startLinks);
+		common.retainAll(endLinks);
+		if (common.size() > 0) {
+			// TODO: Return all elements, just one?
+			path.push(common.get(0));
+			path.push(end);
+			
+			return new Separation(2, path);
+		}
+		
+		// main loop TODO
+		
+		return new Separation(0, new Stack<String>());
+	}
+	
+	public static void main(String[] args) {
 		try {
-			 boLinks = WikiAPI.getAllLinks("Barack Obama");
-			 boBacklinks = WikiAPI.getAllBacklinks("Barack Obama");
+			Separation s1 = findConnection("Apple", "Orange (fruit)");
+			Separation s2 = findConnection("Barack Obama", "Donald Trump");
+			
+			System.out.println(s1.getNumDegrees());
+			System.out.println(s1.getPath());
+			
+			System.out.println(s2.getNumDegrees());
+			System.out.println(s2.getPath());
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		double b = System.currentTimeMillis();
-		
-		int totalBoLinks = 0;
-		int totalBoBacklinks = 0;
-		
-		for (ArrayList<String> listN : boLinks) {
-			System.out.println("List Size: " + listN.size());
-			
-			for (String s : listN) {
-				totalBoLinks++;
-				System.out.println("\t" + s);
-			}
-			
-			System.out.println("\n");
-		}
-		
-		for (ArrayList<String> listN : boBacklinks) {
-			System.out.println("List Size: " + listN.size());
-			
-			for (String s : listN) {
-				totalBoBacklinks++;
-				System.out.println("\t" + s);
-			}
-			
-			System.out.println("\n");
-		}
-		
-		System.out.println("Total Links: " + totalBoLinks);
-		System.out.println("Total Backlinks: " + totalBoBacklinks);
-		System.out.println("Time taken: " + (b - a));
 	}
 }
