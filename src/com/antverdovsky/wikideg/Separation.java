a77ca2242bdc78dcdf43ececb37d2184c42d8b2b
Anatoly Tverdovsky <antverdovsky@gmail.com>
2017-05-04 00:14:07 -0500

    Rewrote Separation for Three or More Degrees
    Rewrote the main loop algorithm for separation of three or more degrees
    to keep the code organized.

--- src/com/antverdovsky/wikideg/Separation.java
+++ src/com/antverdovsky/wikideg/Separation.java
@@ -1,7 +1,6 @@
 package com.antverdovsky.wikideg;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Stack;
@@ -33,7 +32,9 @@ public class Separation {
 	 * @param start The starting article title.
 	 * @param end The ending article title.
 	 * @return A separation instance containing the degrees of separation and
-	 *         the path from the starting article to the ending article.
+	 *         the path from the starting article to the ending article. If no
+	 *         path exists between the start and end article, null will be
+	 *         returned.
 	 * @throws IOException If there occurs an error fetching the articles of
 	 *                     either the starting or ending article. 
 	 */
@@ -60,143 +61,10 @@ public class Separation {
 		separation = Separation.getSeparation2(start, end, links, backlinks);
 		if (separation != null) return separation;
 		
-		return null;
-		
-/*			TODO WIP
-		// Special case if there is some article that is in both the starting
-		// links and the ending backlinks ([start] -> [middle] -> [end]), so
-		// we have two degrees of separation with a one hop chain.
-		HashSet<String> common = new HashSet<String>(startLinks);
-		common.retainAll(endLinks);
-		if (!common.isEmpty()) {
-			// Here, we can grab any element we want, for simplicity lets grab
-			// the first element using the Iterator.
-			path.push(common.iterator().next());
-			path.push(end);
-			
-			return new Separation(degrees, path);
-		}
-		
-		++degrees; // If above failed, we are at >= 3 degrees of separation
-		
-		// As we fetch links to find the midpoint, we are going to need to
-		// store the predecessor for each link, or successor for each 
-		// backlink.
-		HashMap<String, String> predecessors = new HashMap<String, String>();
-		HashMap<String, String> successors = new HashMap<String, String>();
-		
-		// Add each starting link and ending backlink to the predecessors hash 
-		// and successors hash, respectively.
-		for (String s : startLinks) predecessors.put(s, start);
-		for (String s : endLinks) successors.put(s, end);
-		
-		// The main loop which will run until a middle node is found
-		while (true) {
-			// If we have less links from the starting article, lets build
-			// the graph from the starting article.
-			if (startLinks.size() < endLinks.size()) {
-				// Stores the new start links
-				HashSet<String> newStartLinks = new HashSet<String>();
-				
-				// Go through all of the start links and fetch their links.
-				for (String s : startLinks) {
-					// TODO: Optimize so we don't always have to copy over...
-					HashSet<String> sStartLinks = WikiAPI.getAllLinks(s, end);
-					newStartLinks.addAll(sStartLinks);
-					
-					// Set the predecessor of all of the start links
-					for (String sS : sStartLinks) {
-						if (!predecessors.containsKey(sS)) predecessors.put(sS, s);
-					}
-					
-					// If the links we just fetched contain something in
-					// common with the end backlinks, then we found a middle!
-					common = new HashSet<String>(sStartLinks);
-					common.retainAll(endLinks);
-					if (!common.isEmpty()) break;
-				}
-				
-				// Discard the old start links, replacing them with the new 
-				// ones.
-				startLinks = newStartLinks;
-			} 
-			// If we have less links from the ending article, lets build the
-			// graph from the ending article.
-			else {			// TODO: Some code duplication going on here...
-				// Stores the new backlinks
-				HashSet<String> newEndLinks = new HashSet<String>();
-				
-				// Go through all of the backlinks and fetch their backlinks.
-				// Copy all of their backlinks into the new backlinks links.
-				for (String s : endLinks) {
-					// TODO: Optimize so we don't always have to copy over...
-					HashSet<String> sEndLinks = WikiAPI.getAllBacklinks(s, start);
-					newEndLinks.addAll(sEndLinks);
-					
-					// Set the successor of all of the start links
-					for (String sS : sEndLinks) {
-						if (!successors.containsKey(sS)) successors.put(sS, s);
-					}
-					
-					// If the backlinks we just fetched contain something in
-					// common with the start links, then we found a middle!
-					common = new HashSet<String>(sEndLinks);
-					common.retainAll(startLinks);
-					if (!common.isEmpty()) break;
-				}
-				
-				// Discard the old end links, replacing them with the new 
-				// ones.
-				endLinks = newEndLinks;
-			}
-			
-			// Check if there is some element in common between the links and
-			// backlinks list. If there is, we have found the middle!
-			common = new HashSet<String>(startLinks);
-			common.retainAll(endLinks);
-			if (common.size() > 0) {
-				// TODO: Return all elements, just one?
-				// Get the middle element of the Graph
-				String middle = common.iterator().next();
-				
-				// Will store the predecessors and successors of the middle
-				// node element.
-				String curPred = middle;
-				String curSucc = middle;
-				ArrayList<String> midPred = new ArrayList<String>();
-				ArrayList<String> midSucc = new ArrayList<String>();
-				
-				// Get all of the predecessors until the starting element
-				// is reached.
-				while (!curPred.equals(start)) {
-					curPred = predecessors.get(curPred);
-					if (!curPred.equals(start)) midPred.add(curPred);
-				}
-				
-				// Get all of the successors until the starting element
-				// is reached.
-				while (!curSucc.equals(end)) {
-					curSucc = successors.get(curSucc);
-					if (!curSucc.equals(end)) midSucc.add(curSucc);
-				}
-				
-				// Form the link start -> { middle predecessors } -> middle ->
-				// { middle successors } -> end.
-				for (String s : midPred) {
-					path.push(s);
-				}
-				path.push(middle);
-				for (String s : midSucc) {
-					path.push(s);
-				}
-				path.push(end);
-				
-				return new Separation(degrees, path);
-			}
-			
-			++degrees; // For each iteration, the separation increases
-		}
-*/
+		// Last chance is checking for 3+ degrees of separation. If that
+		// returns null also then this path cannot be constructed.
+		separation = Separation.getSeparation3(start, end, links, backlinks);
+		return separation;
 	}
 	
 	/**
@@ -273,8 +141,9 @@ public class Separation {
 	 * of the backlinks of the ending article. 
 	 * @param start The title of the start article.
 	 * @param end The title of the end article.
-	 * @param links The backlinks of the ending article. This hash set should 
-	 *              be empty and will be filled using this method.
+	 * @param links The links of the starting article.
+	 * @param backlinks The backlinks of the ending article. This hash set 
+	 *                  should be empty and will be filled using this method. 
 	 * @return An instance of separation containing the path between start
 	 *         and end, or null if no two degree separation exists.
 	 * @throws IOException If the links could not be properly fetched from the
@@ -305,5 +174,146 @@ public class Separation {
 		path.push(middle);
 		path.push(end);
 		return new Separation(2, path);
+	}
+	
+	/**
+	 * Checks if the separation between the start and end articles is three or
+	 * more degrees of separation. If so, an instance of the separation class 
+	 * is returned. If the separation between the start and end articles is 
+	 * not two, null is returned. A three or more degree separation is only 
+	 * possible if we were unable to find a zero, one, or two degree 
+	 * separation between the start and end articles.  
+	 * @param start The title of the start article.
+	 * @param end The title of the end article.
+	 * @param links The links of the starting article.
+	 * @param backlinks The backlinks of the ending article.
+	 * @return An instance of separation containing the path between start
+	 *         and end, or null if no three+ degree separation exists.
+	 * @throws IOException If the links could not be properly fetched from the
+	 *                     starting article.
+	 */
+	private static Separation getSeparation3(String start, String end,
+			HashSet<String> links, HashSet<String> backlinks) 
+			throws IOException {
+		// As we fetch links to find the midpoint, we are going to need to
+		// store the predecessor for each link, or successor for each 
+		// backlink.
+		HashMap<String, String> predecessors = new HashMap<String, String>();
+		HashMap<String, String> successors = new HashMap<String, String>();
+		
+		// Set the predecessor and successor for each link and backlink.
+		for (String s : links) predecessors.put(s, start);
+		for (String s : backlinks) successors.put(s, end);
+		
+		// The degrees of separation (since this method applies to 3+ degrees
+		// of separation, we can set this to a default value of 3).
+		int degrees = 3;
+		
+		while (true) { // Until we have found a link
+			// Build the graph from the perspective of the smaller data set.
+			if (links.size() <= backlinks.size()) {
+				HashSet<String> newLinks = new HashSet<String>();
+				
+				for (String link : links) { // Go through links
+					// Get all of the links of this link
+					HashSet<String> linksOf = WikiAPI.getAllLinks(link, end);
+					newLinks.addAll(linksOf);
+					
+					// Set this link as the predecessor of all of the links of
+					// this link and add the link to the newLinks list, only
+					// if we have not yet seen this link!
+					for (String linkOf : linksOf)
+						if (!predecessors.containsKey(linkOf))
+							predecessors.put(linkOf, link);
+						
+					// Check if any links of this link are contained in the
+					// backlinks. If so, then this link leads to a backlink
+					// and so it is a middle link and we have found a path!
+					HashSet<String> common = new HashSet<String>(linksOf);
+					common.retainAll(backlinks);
+					if (!common.isEmpty()) break;
+				}
+				
+				// Now replace the old links list with the new links list
+				links = newLinks;
+			} else {
+				HashSet<String> newBacklinks = new HashSet<String>();
+				
+				for (String backlink : backlinks) { // Go through backlinks
+					// Get all of the backlinks of this backlink and add them 
+					// to the new links set.
+					HashSet<String> backlinksOf = 
+							WikiAPI.getAllBacklinks(backlink, start);
+					newBacklinks.addAll(backlinksOf);
+					
+					// Set this backlink as the successor of all of the 
+					// backlinks of this backlink.
+					for (String backlinkOf : backlinksOf)
+						if (!successors.containsKey(backlinkOf))
+							successors.put(backlinkOf, backlink);
+					
+					// Check if any backlinks of this backlink are contained 
+					// in the links. If so, then this backlink leads to a link
+					// and so it is a middle link and we have found a path!
+					HashSet<String> common = new HashSet<String>(backlinksOf);
+					common.retainAll(links);
+					if (!common.isEmpty()) break;
+				}
+				
+				// Now replace the old backlinks list with the new backlinks 
+				// list.
+				backlinks = newBacklinks;
+			}
+			
+			// Check if there is some element in common between the links and
+			// backlinks. If so, then we found a path! Otherwise, we must
+			// repeat the loop though the number of degrees has now increased.
+			HashSet<String> common = new HashSet<String>(links);
+			common.retainAll(backlinks);
+			if (!common.isEmpty()) {
+				// Get some random element from the common list and mark it
+				// as the middle node. Now we need to backtrace through the
+				// predecessors and successors to find the full path from
+				// start to end.
+				String middle = common.iterator().next();
+				
+				// Set the current predecessor / successor of the middle node
+				// to itself, for now.
+				String currentPredecessor = middle;
+				String currentSuccessor = middle;
+				
+				// Create a path stack for storing the path and a queue for
+				// backtracing through all of the predecessors.
+				Stack<String> path = new Stack<String>();
+				Stack<String> backtrace = new Stack<String>();
+				
+				// Until we back trace all the way back to starting title
+				while (!currentPredecessor.equals(start)) {
+					// Get the predecessor of the current predecessor and add
+					// it to the backtrace queue.
+					currentPredecessor = predecessors.get(currentPredecessor);
+					backtrace.push(currentPredecessor);
+				}
+				
+				// Pop all of the titles from the queue and onto the path
+				while (!backtrace.isEmpty()) {
+					path.push(backtrace.pop());
+				}
+				
+				// For the successors, we can just add all of the the
+				// successors to the path stack, including the middle node and
+				// the end article.
+				while (!currentSuccessor.equals(end)) {
+					path.push(currentSuccessor);
+					currentSuccessor = successors.get(currentSuccessor);
+				}
+				path.push(end);
+				
+				// Return the full path with the number of degrees.
+				return new Separation(degrees, path);
+			} else {
+				++degrees;
+			}
+		}
 	}
 }
