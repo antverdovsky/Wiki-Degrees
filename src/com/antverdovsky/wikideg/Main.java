ba70882806bb0eb3a40e03c46f9ca4f3f90e11fd
Anatoly Tverdovsky <antverdovsky@gmail.com>
2017-05-06 14:02:09 -0500

    Started Working on Thread Implementation
    Wrote a very rudimentary thread implementation which will allow us to
    fetch links concurrently, which should greatly speed up the program.

--- src/com/antverdovsky/wikideg/Main.java
+++ src/com/antverdovsky/wikideg/Main.java
@@ -1,9 +1,114 @@
 package com.antverdovsky.wikideg;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Main {
+	static class Downloader implements Runnable {
+		ArrayList<String> myTask;
+		List<String> writeTo;			// thread safe!
+		
+		public Downloader(ArrayList<String> task, List<String> writeTo) {
+			this.myTask = task;
+			this.writeTo = writeTo;
+		}
+		
+		@Override
+		public void run() {
+			for (String s : myTask) {
+				ArrayList<String> linksOf = new ArrayList<String>();
+				try {
+					linksOf = (new LinksFetcher()).getLinks(s, "");
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+//				System.out.println(s);
+				this.writeTo.addAll(linksOf);
+			}
+		}
+	}
+	
+	private static ArrayList<String> singleThread() throws IOException {
+		ArrayList<String> links = (new LinksFetcher()).getLinks("Apple", "");
+		ArrayList<String> ret = new ArrayList<String>();
+		
+		for (String s : links) {
+			ArrayList<String> linksOf = (new LinksFetcher()).getLinks(s, "");
+			System.out.println(s);
+			ret.addAll(linksOf);
+		}
+		
+		return ret;
+	}
+	
+	private static List<String> multiThread() throws IOException {
+		ArrayList<String> links = (new LinksFetcher()).getLinks("Apple", "");
+		List<String> ret = Collections.synchronizedList(new ArrayList<String>());
+		System.out.println(links);
+		
+		ArrayList<Thread> threads = new ArrayList<Thread>();
+		
+		int numThreads = 25;
+		int perThread = links.size() / numThreads;
+		for (int i = 0; i < numThreads; ++i) {
+			int fromIndex = i * perThread;
+			int toIndex = fromIndex + perThread;
+			
+			if (toIndex == 0) {
+				fromIndex = 0;
+				toIndex = links.size();
+			}
+			
+			ArrayList<String> task = new ArrayList<String>(
+					links.subList(fromIndex, toIndex));
+			
+			Thread t = new Thread(new Downloader(task, ret));
+			threads.add(t);
+			t.start();
+		}
+		
+		for (Thread t : threads)
+			try {
+				t.join();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		
+		return ret;
+	}
+	
 	public static void main(String[] args) {
+		int i = 1;
+		
+		if (i == 0) {
+			try {
+				double a = System.currentTimeMillis();
+				ArrayList<String> single = singleThread();
+				
+	//			System.out.println(single);
+				System.out.println(single.size());
+				System.out.println(System.currentTimeMillis() - a);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} else {
+			try {
+				double a = System.currentTimeMillis();
+				List<String> multi = multiThread();
+				
+	//			System.out.println(multi);
+				System.out.println(multi.size());
+				System.out.println(System.currentTimeMillis() - a);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		/*
 		try {
 			Separation s3 =  new Separation("Lingua Franca Nova", "United Airlines");
 			System.out.println(s3.getNumDegrees());
@@ -50,5 +155,6 @@ public class Main {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		*/
 	}
 }
